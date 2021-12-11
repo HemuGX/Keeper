503d441c29f5d45f67bd61e412ac23e492c546c6
Hemu GX <gxhemu@gmail.com>
2021-12-11 15:29:25 +0600

    Keeper Level 1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//1. Create a new React app.
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./components/App"
+
+ReactDOM.render(<App /> , document.getElementById("root") )
